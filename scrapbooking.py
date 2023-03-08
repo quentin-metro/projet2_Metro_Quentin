8fd4550bbfeac10023efbebc889bc64ccb34cb94
quentin-metro <quentin.metro@gmail.com>
2023-03-08 14:18:31 +0100

    Modifies le csv de sortie , intervertie colonne et ligne

--- scrapbooking.py
+++ scrapbooking.py
@@ -2,34 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+# Récupérer la page d'un produit et la 'parser'
 url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 page = requests.get(url)
 if page.ok:
     soup = BeautifulSoup(page.content, 'html.parser')
-    # Créer un nouveau fichier pour écrire dans le fichier appelé « data.csv »
-    # Créer une liste pour les en-têtes
+
+    # Organisation du CSV
     with open('Metro_Quentin_2_data_032023.csv', 'w') as fichier_csv:
         writer = csv.writer(fichier_csv, delimiter=',')
-        en_tete = ["categorie", "valeur"]
-        writer.writerow(en_tete)
-        ligne_url = ["URL Produit", url]
-        writer.writerow(ligne_url)
+        en_tete = []
+        data_livre = []
+
         # Récupération du titre d'un produit
-        titre = ["titre", soup.find(class_="active").string]
-        writer.writerow(titre)
+        en_tete.append("Titre")
+        titre = soup.find(class_="active")
+        data_livre.append(titre.string)
+
+        # Récupération url
+        en_tete.append("URL")
+        data_livre.append(url)
 
         # recuperation imageURL
+        en_tete.append("Image")
         images = soup.find_all('img')
         for image in images:
-            ligne_image = ["image", image['src']]
-            writer.writerow(ligne_image)
+            data_livre.append(image['src'])
 
         # recuperation desc
-        desc = ["Description", soup.find('p', class_='').string]
-        writer.writerow(desc)
+        en_tete.append("Product Description")
+        desc = soup.find('p', class_='')
+        data_livre.append(desc.string)
+
         # Recuperation UPC,prices, availability, type, score
         tableau = soup.find_all('tr')
         for ligne in tableau:
             if ligne.find('th').string != "Tax":
-                ligne_valeur = [ligne.find('th').string, ligne.find('td').string]
-                writer.writerow(ligne_valeur)
+                en_tete.append(ligne.find('th').string)
+                data_livre.append(ligne.find('td').string)
+
+        writer.writerow(en_tete)
+        writer.writerow(data_livre)
